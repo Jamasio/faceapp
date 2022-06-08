4c8f8d47ae114a8894da770ec34fab41aa7deaef
Jamasio <jamasioromero@gmail.com>
2022-06-08 18:26:25 +0100

    adding API

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
 
-const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
   apiKey: '09a022f16b24487fbddf9a140ac7028a'
@@ -26,7 +26,7 @@ class App extends Component {
   onButtonSubmit = () => {
     app.models.predict("09a022f16b24487fbddf9a140ac7028a", "https://samples.clarifai.com/face-det.jpg").then(
       function(response) {
-
+        console.log();
       },
       function(err) {
 
