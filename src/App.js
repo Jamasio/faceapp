6cdca7e5718e459184a2e463c0d99e128eb4be8a
Jamasio <jamasioromero@gmail.com>
2022-07-11 17:01:10 +0100

    Adding particles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,85 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Clarifai from 'clarifai';
 import Siginin from './components/Signin/Signin';
 import Register from './components/Register/Register';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
+const particlesInit = async (main) => {
+  console.log(main);
+  await loadFull(main);
+};
+
+const particlesLoaded = (container) => {
+  console.log(container);
+};
+
+const particlesOptions = {
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 300,
+      enable: true,
+      opacity: 0.5,
+      width: 0.5,
+    },
+    collisions: {
+      enable: false,
+    },
+    move: {
+      direction: "left",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 50,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+}
 
 const app = new Clarifai.App({
   apiKey: '09a022f16b24487fbddf9a140ac7028a'
@@ -101,6 +179,11 @@ class App extends Component {
    const { isSignedIn, imageUrl, route, box, user } = this.state;
     return (
       <div className="App">
+         <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={particlesOptions} className="particles"/>
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home'
           ? <div>
